59ac280cd26455d581241da3cf137a2e2b58a788
habibiealaydrus <habibiemohamad@gmail.com>
2022-06-30 19:51:49 +0700

    membelah menjadi mvc

---
+++ application/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+class Home extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('template/header');
+        $this->load->view('home/index');
+        $this->load->view('template/footer');
+    }
+}
