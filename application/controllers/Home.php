13c0ade78cf61f8dc0ae8eda81ea420c4b73dd34
habibiealaydrus <habibiemohamad@gmail.com>
2022-07-20 19:55:34 +0700

    adding some template

---
+++ application/controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('layout/header');
+        $this->load->view('layout/about');
+        $this->load->view('layout/quote');
+        $this->load->view('layout/services');
+        $this->load->view('layout/features');
+        $this->load->view('layout/pricingplan');
+        $this->load->view('layout/team');
+        $this->load->view('layout/testimonial');
+        $this->load->view('layout/footer');
+    }
+}
